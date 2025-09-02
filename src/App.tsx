f1d9e22865448ac6fb59eaaa4ed2c7ba468eaca8
wo-o29 <woogur29@gmail.com>
2025-09-02 21:52:32 +0900

    feat: 코드 스플리팅 추가(`lazy` + `Suspense`)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
-
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
+import { lazy, Suspense } from 'react';
+
+const HomePage = lazy(() => import('./pages/Home/Home'));
+const SearchPage = lazy(() => import('./pages/Search/Search'));
 
 import './App.css';
 
@@ -13,8 +13,22 @@ const App = () => {
     <Router basename={'/perf-basecamp'}>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<div>Home Page Loading...</div>}>
+              <HomePage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Suspense fallback={<div>Search Page Loading...</div>}>
+              <SearchPage />
+            </Suspense>
+          }
+        />
       </Routes>
       <Footer />
     </Router>
