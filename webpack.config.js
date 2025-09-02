2b8be441ba81a443cb90e0b0cfd3a2e403b2238e
wo-o29 <woogur29@gmail.com>
2025-09-02 20:09:54 +0900

    chore: openAnalyzer 속성 off

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -24,7 +25,14 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [{ from: './public', to: './public' }]
     }),
-    new Dotenv()
+    new Dotenv(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static', // 'server', 'static', 'disabled' 중 선택. static은 html 파일 생성
+      openAnalyzer: false,
+      generateStatsFile: true, // json 통계 파일 생성
+      statsFilename: 'bundle-stats.json', // json 파일명 지정
+      reportFilename: 'bundle-report.html' // html 보고서 파일명 지정
+    })
   ],
   module: {
     rules: [
